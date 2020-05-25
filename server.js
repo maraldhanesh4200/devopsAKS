d67d49bbb176b06de219cfa9570d46ab73254ec6
maraldhanesh4200 <65558023+maraldhanesh4200@users.noreply.github.com>
2020-05-25 20:02:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world Of Azure Devops\n');
+  res.send('Hello world Of Azure Devops with AKS\n');
 });
 
 app.listen(PORT, HOST);
